98769eb1576d1ff0308da53cee3643449d94c6b2
Anushanallap <anusha.n468@gmail.com>
2021-12-24 12:29:43 +0530

    added UC3 file

--- Hashtable.java
+++ Hashtable.java
@@ -16,7 +16,7 @@ public class Hashtable<k, v> {
 	        }
 	    }
 
-	    
+	    //Append the value in the linked list
 	    public void append(INode myNode) {
 	        if(this.head == null)
 	            this.head = myNode;
@@ -28,7 +28,7 @@ public class Hashtable<k, v> {
 	        }
 	    }
 
-	    
+	    //Searching for the word in the linked list
 	    public INode<k, v> searchNode(k data) {
 	    	INode currentNode = head;
 	        int position = 0;
@@ -42,13 +42,13 @@ public class Hashtable<k, v> {
 	        return currentNode;
 	    }
 
-	    
+	    //Searching for the word and get the value from the linked list
 	    public v get(k word) {
 	    	INode<k, v> myMapNode = searchNode(word);
 	        return (myMapNode == null) ? null : myMapNode.getValue();
 	    }
 
-	    
+	    //Print the linked list
 	    @Override
 	    public String toString() {
 	        return "MyLinkedListNodes{" + head + "}";
@@ -57,7 +57,29 @@ public class Hashtable<k, v> {
 	    public void printNodes() {
 	        System.out.println("My nodes: " + head);
 	    }
+
+		public void remove(String string) {
+			    {
+			        INode currentNode = head;
+			        INode previousNode = null;
+			        while (currentNode != null && currentNode.getKey().equals("word")) {
+			            head = currentNode.getNext();
+			            return;
+			        }
+			        while (currentNode != null && !(currentNode.getKey().equals("word"))) {
+			            previousNode = currentNode;
+			            currentNode = currentNode.getNext();
+			        }
+			        if (currentNode != null) {
+			            previousNode.next = currentNode.next;
+			        }
+			        if(currentNode == null)
+			            System.out.println(" Word not found! ");
+			    }
+			
+		}
 	}
 	
+
 
 
